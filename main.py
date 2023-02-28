ae2c180753312e862bdf2213a57a51747d8de17a
Julius Nguyen <92987090+julius-nguyen@users.noreply.github.com>
2023-02-28 11:28:42 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from question_model import Question
+from quiz_brain import QuizBrain
+from boolean_ui import BoolQuizInterface
+from setup_ui import ConfigQuizInterface
+import requests
+
+question_bank = []
+
+config = True 
+while config:
+    cfg_ui = ConfigQuizInterface()
+    if cfg_ui.parameters != {}:
+        config = False 
+
+mode = cfg_ui.selected_mode
+
+response = requests.get('https://opentdb.com/api.php',params=cfg_ui.parameters)
+response.raise_for_status()
+
+cfg_ui.destroy()
+
+question_data = response.json()['results']
+
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+quiz_ui = BoolQuizInterface(quiz)
