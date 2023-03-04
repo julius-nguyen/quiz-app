7551a13c1a85f4e75c8f08dfbd5a979af23ed6f3
Julius Nguyen <92987090+julius-nguyen@users.noreply.github.com>
2023-03-04 08:49:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from quiz_brain import QuizBrain
 from boolean_ui import BoolQuizInterface
+from mc_ui import MCQuizInterface
 from setup_ui import ConfigQuizInterface
 import requests
 
@@ -13,21 +14,30 @@ while config:
         config = False 
 
 mode = cfg_ui.selected_mode
+quiz_type = cfg_ui.q_type
 
-response = requests.get('https://opentdb.com/api.php',params=cfg_ui.parameters)
+print(cfg_ui.parameters)
+
+response = requests.get('https://opentdb.com/api.php?',params=cfg_ui.parameters)
 response.raise_for_status()
 
 cfg_ui.destroy()
 
 question_data = response.json()['results']
-print(question_data)
 
 for question in question_data:
+    print(question)
     question_text = question["question"]
     question_answer = question["correct_answer"]
-    new_question = Question(question_text, question_answer)
+    question_incorrect_answers = question["incorrect_answers"]
+    new_question = Question(question_text, question_answer,question_incorrect_answers)
     question_bank.append(new_question)
 
 
 quiz = QuizBrain(question_bank)
-quiz_ui = BoolQuizInterface(quiz)
+
+if quiz_type == 'boolean':
+    quiz_interface = BoolQuizInterface(quiz)
+if quiz_type == 'multiple':
+    quiz_interface = MCQuizInterface(quiz)
+
