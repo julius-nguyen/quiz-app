b542b61a9d0e126a15ac5323e28d72366907d97a
Julius Nguyen <92987090+julius-nguyen@users.noreply.github.com>
2023-03-04 08:50:06 +0100

    Update question_model.py

--- question_model.py
+++ question_model.py
@@ -1,5 +1,6 @@
 class Question:
 
-    def __init__(self, q_text, q_answer):
+    def __init__(self, q_text, q_correct_answer,q_incorrect_answers):
         self.text = q_text
-        self.answer = q_answer
+        self.correct_answer = q_correct_answer
+        self.incorrect_answers = q_incorrect_answers
